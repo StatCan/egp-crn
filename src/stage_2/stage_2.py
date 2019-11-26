52f68de4d68e9344f8761e301eb860ee87a3b899
KentJacobs <kent.jacobs@canada.ca>
2019-11-26 09:56:17 -0500

    addition of command line arguments (input and output gpkg)

--- src/stage_2/stage_2.py
+++ src/stage_2/stage_2.py
@@ -16,56 +16,64 @@ db = 'nrn'
 user = 'postgres'
 port = 5432
 pwd = 'password'
-
-# read the incoming geopackage from stage 1
-# gpkg = gpd.read_file("data/interim/.gpkg")
-
-# convert the stage 1 geopackage to a shapefile for networkx usage
-# gpkg.to_file("data/interim/.shp", driver='ESRI Shapefile')
-
-input = "C:/Users/jacoken/data/NRN/NRN_RRN_ON_12_0_SHAPE/NRN_ON_12_0_SHAPE_en/NRN_ON_12_0_ROADSEG.shp"
-
-# read shapefile
-graph = nx.read_shp(os.path.abspath(input))
-
-# create geodataframe for graph
-graph_gpd = gpd.read_file(input)
-
 # postgres database url
 db_url = URL(drivername='postgresql+psycopg2', host=host, database=db, username=user, port=port, password=pwd)
-
 # engine to connect to postgres
 engine = create_engine(db_url)
 
-# import graph into postgis
-graph_gpd.postgis.to_postgis(engine, 'stage_2', 'LineString', if_exists='replace')
+def main(gpkg_in, gpkg_out):
 
-# create empty graph for dead ends
-g_dead_ends = nx.Graph()
+    gpkg_in = (sys.argv[1])
+    gpkg_out = (sys.argv[2])
 
-# filter for dead ends
-dead_ends_filter = [node for node, degree in graph.degree() if degree == 0 or degree == 1]
+    # read the incoming geopackage from stage 1
+    gpkg_in = gpd.read_file(gpkg_in)
+    # convert the stage 1 geopackage to a shapefile for networkx usage
+    gpkg_in.to_file("data/interim/netx1.shp", driver='ESRI Shapefile')
+    # read shapefile
+    graph = nx.read_shp("data/interim/netx1.shp")
+    # create geodataframe for graph
+    graph_gpd = gpd.read_file("data/interim/netx1.shp")
 
-# add filter to empty graph
-g_dead_ends.add_nodes_from(dead_ends_filter)
+    # import graph into postgis
+    graph_gpd.postgis.to_postgis(engine, 'stage_2', 'LineString', if_exists='replace')
 
-# SQL query to create junctions (JUNCTYPE=Intersection)
-sql = "WITH inter AS (SELECT ST_Intersection(a.geom, b.geom) geom, " \
-                  "Count(DISTINCT a.index) " \
-           "FROM   stage_2 AS a, " \
-                  "stage_2 AS b " \
-           "WHERE  ST_Touches(a.geom, b.geom) " \
-                  "AND a.index != b.index " \
-           "GROUP  BY ST_Intersection(a.geom, b.geom))" \
-      "SELECT * FROM inter WHERE count > 2;"
+    # create empty graph for dead ends
+    g_dead_ends = nx.Graph()
+    # filter for dead ends
+    dead_ends_filter = [node for node, degree in graph.degree() if degree == 0 or degree == 1]
+    # add filter to empty graph
+    g_dead_ends.add_nodes_from(dead_ends_filter)
 
-# create junctions geodataframe
-inter = gpd.GeoDataFrame.from_postgis(sql, engine)
+    # SQL query to create junctions (JUNCTYPE=Intersection)
+    sql = "WITH inter AS (SELECT ST_Intersection(a.geom, b.geom) geom, " \
+                      "Count(DISTINCT a.index) " \
+               "FROM   stage_2 AS a, " \
+                      "stage_2 AS b " \
+               "WHERE  ST_Touches(a.geom, b.geom) " \
+                      "AND a.index != b.index " \
+               "GROUP  BY ST_Intersection(a.geom, b.geom))" \
+          "SELECT * FROM inter WHERE count > 2;"
 
-nx.write_shp(g_dead_ends, os.path.abspath("data/interim/dead_end.shp"))
-dead_ends_gpd = gpd.read_file("data/interim/dead_end.shp")
-dead_ends_gpd.to_file("data/interim/junctions.gpkg", layer='deadends', driver='GPKG')
-inter.to_file("data/interim/junctions.gpkg", layer='intersections', driver='GPKG')
+    # create junctions geodataframe
+    inter = gpd.GeoDataFrame.from_postgis(sql, engine)
+
+    nx.write_shp(g_dead_ends, "data/interim/dead_end.shp")
+    dead_ends_gpd = gpd.read_file("data/interim/dead_end.shp")
+
+    dead_ends_gpd.to_file(gpkg_out, layer='deadends', driver='GPKG')
+    inter.to_file(gpkg_out, layer='intersections', driver='GPKG')
+
+if __name__ == "__main__":
+    #
+    # example run : $ python stage_2.py [INPUT GPKG] [OUTPUT GPKG]
+    #
+
+    if len( sys.argv ) != 3:
+        print("ERROR: You must supply 7 arguments. Example: python stage_2.py [INPUT GPKG] [OUTPUT GPKG]")
+        sys.exit( 1 )
+
+    main(sys.argv[1], sys.argv[2])
 
 # output execution time
 print("Total execution time: ", datetime.now() - startTime)
