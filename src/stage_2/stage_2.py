965c573ad45f7495bd93e27289b53465567fdbff
KentJacobs <kent.jacobs@canada.ca>
2019-12-13 16:08:06 -0500

    code clean up and sql yaml addition

--- src/stage_2/stage_2.py
+++ src/stage_2/stage_2.py
@@ -14,7 +14,6 @@ from geopandas_postgis import PostGIS
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 import helpers
 import logging
-from yaml import safe_load as yload
 
 # Set logger.
 logger = logging.getLogger()
@@ -59,45 +58,100 @@ class Stage:
         # print(self.dframes["roadseg"])
 
     def gen_dead_end(self):
-        """Generates dead end junction types."""
+        """Generates dead end junctions with NetworkX."""
 
         logging.info("Converting roadseg dataframe to shapefile for NetworkX.")
-        self.df = gpd.GeoDataFrame(self.dframes["roadseg"], geometry='geometry')
-        self.df.to_file("../../data/raw/netx.shp", driver="ESRI Shapefile")
-        self.df.crs = {'init': 'epsg:4617'}
+        self.df_roadseg = gpd.GeoDataFrame(self.dframes["roadseg"], geometry='geometry')
+        self.df_roadseg.to_file("../../data/raw/netx.shp", driver="ESRI Shapefile")
+        self.df_roadseg.crs = {'init': 'epsg:4617'}
 
-        logging.info("Read generated shapefile for dead end creation.")
+        logging.info("Read generated shapefiles for dead end creation.")
         graph = nx.read_shp("../../data/raw/netx.shp")
 
-        logging.info("Create an empty graph for junction dead ends.")
-        g_dead_ends = nx.Graph()
+        logging.info("Create an empty graph for dead ends junctions.")
+        dead_ends = nx.Graph()
 
         logging.info("Filter for dead end junctions.")
         dead_ends_filter = [node for node, degree in graph.degree() if degree == 1]
 
         logging.info("Insert filtered dead end junctions into empty graph.")
-        g_dead_ends.add_nodes_from(dead_ends_filter)
+        dead_ends.add_nodes_from(dead_ends_filter)
 
         logging.info("Write dead end junctions shapefile.")
-        nx.write_shp(g_dead_ends, "../../data/raw/dead_end.shp")
+        nx.write_shp(dead_ends, "../../data/raw/dead_end.shp")
+
+        logging.info("Create dead end geodataframe.")
+        self.dead_end_gdf = gpd.read_file("../../data/raw/dead_end.shp")
+        self.dead_end_gdf["junctype"] = "Dead End"
+        print(self.dead_end_gdf)
 
     def gen_intersections(self):
         """Generates intersection junction types."""
 
         logging.info("Importing roadseg geodataframe into PostGIS.")
-        self.df.postgis.to_postgis(con=engine, table_name="stage_2", geometry="LineString", if_exists="replace")
+        self.df_roadseg.postgis.to_postgis(con=engine, table_name="stage_2", geometry="LineString", if_exists="replace")
 
-        sql = load_yaml("../sql.yaml")
+        logging.info("Loading SQL yaml.")
+        sql = helpers.load_yaml("../sql.yaml")
 
         inter_filter = sql["intersections"]["filter"]
 
         logging.info("Executing SQL injection for junction intersections.")
-        inter_gdf = gpd.GeoDataFrame.from_postgis(inter_filter, engine)
-        # inter_df = pd.DataFrame(inter_gdf)
+        self.inter_gdf = gpd.GeoDataFrame.from_postgis(inter_filter, engine, geom_col="geometry")
+        self.inter_gdf["junctype"] = "Intersection"
+        self.inter_gdf.crs = {'init': 'epsg:4617'}
+        # inter_df = pd.DataFrame(self.inter_gdf)
+        print(self.inter_gdf)
 
         logging.info("Writing junction intersection GeoPackage.")
-        inter_gdf.to_file("../../data/raw/intersections.gpkg", driver="GPKG")
+        # inter_gdf.to_file("../../data/raw/intersections.gpkg", driver="GPKG")
         # helpers.export_gpkg(inter_gpd, "../../data/raw/intersections2.gpkg")
+
+    def gen_ferry(self):
+        """Generates ferry junctions with NetworkX."""
+
+        logging.info("Converting ferryseg dataframe to shapefile for NetworkX.")
+        self.df_ferryseg = gpd.GeoDataFrame(self.dframes["ferryseg"], geometry='geometry')
+        self.df_ferryseg.to_file("../../data/raw/netx_ferry.shp", driver="ESRI Shapefile")
+        self.df_ferryseg.crs = {'init': 'epsg:4617'}
+
+        logging.info("Read generated shapefile for ferry junction creation.")
+        graph_ferry = nx.read_shp("../../data/raw/netx_ferry.shp")
+
+        logging.info("Create an empty graph for ferry junctions.")
+        ferry_junc = nx.Graph()
+
+        logging.info("Filter for ferry junctions.")
+        ferry_filter = [node for node, degree in graph_ferry.degree() if degree > 0]
+
+        logging.info("Insert filtered ferry junctions into empty graph.")
+        ferry_junc.add_nodes_from(ferry_filter)
+
+        logging.info("Write ferry junctions shapefile.")
+        nx.write_shp(ferry_junc, "../../data/raw/ferry_dead_end.shp")
+
+        logging.info("Create ferry geodataframe.")
+        self.ferry_gdf = gpd.read_file("../../data/raw/ferry.shp")
+        self.ferry_gdf["junctype"] = "Ferry"
+        self.ferry_gdf.crs = {'init': 'epsg:4617'}
+        print(self.ferry_gdf)
+
+    def combine(self):
+        """Combine geodataframes."""
+
+        logging.info("Combining ferry, dead end and intersection junctions.")
+        junctions = gpd.GeoDataFrame(pd.concat([self.ferry_gdf, self.dead_end_gdf, self.inter_gdf], sort=False))
+        junctions = junctions[['junctype', 'geometry']]
+        junctions.crs = {'init': 'epsg:4617'}
+        print(junctions)
+
+        junctions["geometry"] = [MultiPoint([feature]) if type(feature) == Point else feature for feature in junctions["geometry"]]
+        self.ferry_gdf["geometry"] = [MultiPoint([feature]) if type(feature) == Point else feature for feature in self.ferry_gdf["geometry"]]
+
+        logging.info("Importing merged junctions into PostGIS.")
+        junctions.postgis.to_postgis(con=engine, table_name='stage_2_junc', geometry='MULTIPOINT', if_exists='replace')
+        self.ferry_gdf.postgis.to_postgis(con=engine, table_name='stage_2_ferry_junc', geometry='MULTIPOINT', if_exists='replace')
+
 
     # def junctions(self):
     #
@@ -260,6 +314,8 @@ class Stage:
         self.load_gpkg()
         self.gen_dead_end()
         self.gen_intersections()
+        self.gen_ferry()
+        self.combine()
 
 
 def main():
