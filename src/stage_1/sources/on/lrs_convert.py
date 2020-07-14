6e4e7aca2a61ce580cffe06041dc7bc48d02052e
Jesse Stewart <jesse-stewart18@hotmail.com>
2020-07-14 16:06:36 -0400

    [1] implemented ON lrs_convert functions resolve_unsplit_parities and reduce_events.

--- src/stage_1/sources/on/lrs_convert.py
+++ src/stage_1/sources/on/lrs_convert.py
@@ -1,6 +1,7 @@
 import click
 import geopandas as gpd
 import logging
+import numpy as np
 import os
 import pandas as pd
 import sys
@@ -26,6 +27,13 @@ class ORN:
         self.base_query = "road_element_type != 'VIRTUAL ROAD'"
         self.base_fk = "ogf_id"
         self.source_fk = "orn_road_net_element_id"
+        self.event_measurement_fields = ["from_measure", "to_measure"]
+        self.irreducible_datasets = ["orn_road_net_element", "orn_blocked_passage", "orn_toll_point"]
+        self.parities = {
+            "orn_address_info": "street_side",
+            "orn_jurisdiction": "street_side",
+        }
+        self.address_dataset = "orn_address_info"
 
         # Validate src.
         self.src = os.path.abspath(src)
@@ -178,15 +186,137 @@ class ORN:
                 else:
                     del self.source_datasets[name]
 
-    def group_paritized_attributes(self):
-        """Groups records which are split by parity (left, right, both)."""
+    def resolve_unsplit_parities(self):
+        """
+        For paritized attributes, duplicates records where the parity field = 'Both' into 'Left' and 'Right'. This
+        makes it easier to reduce lrs attributes.
+        """
 
-        # TODO
+        logger.info("Resolving unsplit parities.")
 
-    def reduce_attributes(self):
-        """Reduces duplicated base dataset events to the event with the longest measurement."""
+        # Iterate paritized datasets and fields.
+        for table, field in self.parities.items():
 
-        # TODO
+            logger.info(f"Resolving unsplit parities for dataset: {table}, field: {field}.")
+
+            df = self.source_datasets[table].copy(deep=True)
+
+            # Copy unsplit records and update as "Right".
+            right_side = df[df[field] == "Both"].copy(deep=True)
+            right_side.loc[right_side.index, field] = "Right"
+
+            # Update original records as "Left".
+            df.loc[df[field] == "Both", field] = "Left"
+
+            # Concatenate right-side attributes to original dataframe.
+            df = pd.concat([df, right_side], ignore_index=True)
+
+            # Store results.
+            if "geometry" in df.columns:
+                self.source_datasets[table] = gpd.GeoDataFrame(df.copy(deep=True))
+            else:
+                self.source_datasets[table] = df.copy(deep=True)
+
+    def reduce_events(self):
+        """
+        Reduces many-to-one base dataset events to the event with the longest measurement.
+        Exception: paritized fields keep the longest event for both "Left" and "Right" instances.
+        """
+
+        def configure_address_structure(structures):
+            """Configures the address structure given an iterable of structures."""
+
+            structures = set(structures)
+
+            if len(structures) == 1:
+                return list(structures)[0]
+            elif "Unknown" in structures:
+                return "Unknown"
+            elif "Irregular" in structures:
+                return "Irregular"
+            elif "Mixed" in structures or {"Even", "Odd"}.issubset(structures):
+                return "Mixed"
+            else:
+                return "Unknown"
+
+        logger.info("Reducing events.")
+
+        # Iterate datasets, excluding the base and irreducible datasets.
+        for name, df in self.source_datasets.items():
+            if name not in {self.base_dataset, *self.irreducible_datasets}:
+
+                logger.info(f"Reducing events for dataset: {name}.")
+
+                # Calculate event lengths.
+                df["event_length"] = np.abs(df[self.event_measurement_fields[0]] - df[self.event_measurement_fields[1]])
+
+                # Handle paritized fields.
+                if name in self.parities:
+
+                    dfs = list()
+                    for parity in ("Left", "Right"):
+
+                        logger.info(f"Paritized dataset detected. Reducing dataset for parity: {parity}.")
+
+                        # Get parity records.
+                        records = df[df[self.parities[name]] == parity].copy(deep=True)
+
+                        # Handle address ranges.
+                        address_attributes = dict()
+                        if name == self.address_dataset:
+
+                            logger.info("Address dataset detected. Configuring updated address attributes.")
+
+                            # Configure updated address attributes.
+                            address_attributes = {
+                                "first_house_number": helpers.groupby_to_list(
+                                    records, self.source_fk, "first_house_number").map(min),
+                                "last_house_number": helpers.groupby_to_list(
+                                    records, self.source_fk, "last_house_number").map(max),
+                                "house_number_structure": helpers.groupby_to_list(
+                                    records, self.source_fk, "house_number_structure").map(configure_address_structure),
+                                "effective_datetime": helpers.groupby_to_list(
+                                    records, self.source_fk, "effective_datetime").map(max)
+                            }
+
+                        # Drop duplicate events, keeping the maximum event_length.
+                        records = records.sort_values("event_length").drop_duplicates(self.source_fk, keep="last")
+
+                        # Update address attributes, if possible.
+                        if len(address_attributes):
+
+                            records.index = records[self.source_fk]
+                            for attribute, series in address_attributes.items():
+
+                                logger.info(f"Updating address attribute: {attribute}.")
+
+                                # Update attribute.
+                                records[attribute].update(series)
+
+                            records.reset_index(drop=True, inplace=True)
+
+                        dfs.append(records)
+
+                    # Concatenate records.
+                    df = pd.concat(dfs, ignore_index=True)
+
+                else:
+
+                    # Drop duplicate events, keeping the maximum event_length.
+                    df = df.sort_values("event_length").drop_duplicates(self.source_fk, keep="last")
+
+                # Drop event measurement fields.
+                df.drop(columns=["event_length", *self.event_measurement_fields], inplace=True)
+
+                # Log changes.
+                logger.info(f"Dropped {len(self.source_datasets[name]) - len(df)} of {len(self.source_datasets[name])} "
+                            f"records for dataset: {name}.")
+
+                # Store results.
+                if "geometry" in df.columns:
+                    self.source_datasets[name] = gpd.GeoDataFrame(df.copy(deep=True))
+                else:
+                    self.source_datasets[name] = df.copy(deep=True)
 
     def execute(self):
         """Executes class functionality."""
@@ -194,8 +324,8 @@ class ORN:
         self.compile_source_datasets()
         self.configure_secondary_linkages()
         self.configure_valid_records()
-        self.group_paritized_attributes()
-        self.reduce_attributes()
+        self.resolve_unsplit_parities()
+        self.reduce_events()
         self.assemble_base_dataset()
 
 
