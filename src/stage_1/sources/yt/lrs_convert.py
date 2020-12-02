f9d2be1fc30af60ed624d402f5c88d57d2d14379
Jesse Stewart <jesse-stewart18@hotmail.com>
2020-12-02 16:31:08 -0400

    [1] reconfigured init and df compilation logic for yukon lrs_convert.py

--- src/stage_1/sources/yt/lrs_convert.py
+++ src/stage_1/sources/yt/lrs_convert.py
@@ -1,4 +1,5 @@
 import click
+import fiona
 import geopandas as gpd
 import logging
 import numpy as np
@@ -34,6 +35,59 @@ class LRS:
     def __init__(self, src, dst):
         self.nrn_datasets = dict()
         self.source_datasets = dict()
+        self.event_measurement_fields = ["fromkm", "tokm"]
+        self.schema = {
+            "br_bridge_ln": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "bridge_name"],
+                "query": "todate.isna()"
+            },
+            "sm_structure": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "surface_code"],
+                "query": "todate.isna()"
+            },
+            "tdylrs_centerline": {
+                "fields": ["centerlineid", "geometry"],
+                "query": "todate.isna() and (networkid == 1)"
+            },
+            "tdylrs_centerline_sequence": {
+                "fields": ["centerlineid", "fromdate", "todate", "networkid", "routeid", "centerlineid"],
+                "query": "todate.isna()"
+            },
+            "tdylrs_primary_rte": {
+                "fields": ["fromdate", "todate", "routeid", "planimetric_accuracy", "acquisition_technique_dv",
+                           "acquired_by_dv", "acquisition_date"],
+                "query": "todate.isna()"
+            },
+            "td_lane_configuration": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "lane_configuration"],
+                "query": "todate.isna()"
+            },
+            "td_number_of_lanes": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "number_of_lanes"],
+                "query": "todate.isna()"
+            },
+            "td_road_administration": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "administration"],
+                "query": "todate.isna()"
+            },
+            "td_road_type": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "road_type"],
+                "query": "todate.isna()"
+            },
+            "td_street_name": {
+                "fields": ["routeid", "fromdate", "todate", "fromkm", "tokm", "street_direction_prefix",
+                           "street_type_prefix", "street_name", "street_type_suffix", "street_direction_suffix"],
+                "query": "todate.isna()"
+            }
+        }
+        self.structure = {
+            "base": "tdylrs_centerline_sequence",
+            "connections": {
+                "centerlineid": ["tdylrs_centerline_sequence"],
+                "routeid": ["br_bridge_ln", "sm_structure", "tdylrs_primary_rte", "td_lane_configuration",
+                            "td_number_of_lanes", "td_road_administration", "td_road_type", "td_street_name"]
+            }
+        }
 
         # Validate src.
         self.src = os.path.abspath(src)
@@ -49,10 +103,174 @@ class LRS:
         if os.path.exists(self.dst):
             logger.exception(f"Invalid dst input: {dst}. File already exists.")
 
+    def compile_source_datasets(self):
+        """Loads source layers into (Geo)DataFrames."""
+
+        logger.info(f"Compiling source datasets from: {self.src}.")
+
+        rename = {
+            "acquired_by_dv": "provider",
+            "acquisition_date": "credate",
+            "acquisition_technique_dv": "acqtech",
+            "administration": "roadjuris",
+            "bridge_name": "strunameen",
+            "fromdate": "revdate",
+            "lane_configuration": "trafficdir",
+            "number_of_lanes": "nbrlanes",
+            "planimetric_accuracy": "accuracy",
+            "road_type": "roadclass",
+            "street_direction_prefix": "dirprefix",
+            "street_direction_suffix": "dirsuffix",
+            "street_name": "namebody",
+            "street_type_prefix": "strtypre",
+            "street_type_suffix": "strtysuf",
+            "surface_code": "pavstatus"
+        }
+
+        # Compile layer names for lowercase lookup.
+        layers_lower = {name.lower(): name for name in fiona.listlayers(self.src)}
+
+        # Iterate LRS schema.
+        for index, items in enumerate(self.schema.items()):
+
+            layer, cols = items
+
+            logger.info(f"Compiling source layer {index + 1} of {len(self.schema)}: {layer}.")
+
+            # Load layer into dataframe, force lowercase column names.
+            df = gpd.read_file(self.src, driver="OpenFileGDB", layer=layers_lower[layer]).rename(columns=str.lower)
+
+            # Filter columns.
+            df.drop(columns=df.columns.difference(cols), inplace=True)
+
+            # Update column names to match NRN.
+            df.rename(columns=rename, inplace=True)
+
+            # Convert tabular dataframes.
+            if "geometry" not in df.columns:
+                df = pd.DataFrame(df)
+
+            # Store results.
+            self.source_datasets[layer] = df.copy(deep=True)
+
+    def configure_valid_records(self):
+        """Applies dataset queries and keeps only records which link to valid records from the base dataset."""
+
+        logger.info(f"Configuring valid records.")
+
+        # Filter base dataset to valid records.
+        logger.info(f"Configuring valid records for base dataset: {self.base_dataset}.")
+
+        count = len(self.source_datasets[self.base_dataset])
+        self.source_datasets[self.base_dataset].query(self.base_query, inplace=True)
+        logger.info(f"Dropped {count - len(self.source_datasets[self.base_dataset])} of {count} records for base "
+                    f"dataset: {self.base_dataset}.")
+
+        # Compile base dataset foreign keys.
+        base_fkeys = set(self.source_datasets[self.base_dataset][self.base_fk])
+
+        # Iterate dataframes and remove records which do not link to the base dataset.
+        for name, df in self.source_datasets.items():
+            if self.source_fk in df.columns:
+
+                logger.info(f"Configuring valid records for source dataset: {name}.")
+
+                df_valid = df[df[self.source_fk].isin(base_fkeys)]
+                logger.info(f"Dropped {len(df) - len(df_valid)} of {len(df)} records for dataset: {name}.")
+
+                # Store or delete dataset.
+                if len(df_valid):
+                    self.source_datasets[name] = df_valid.copy(deep=True)
+                else:
+                    del self.source_datasets[name]
+
+    def export_gpkg(self):
+        """Exports the NRN datasets to a GeoPackage."""
+
+        logger.info(f"Exporting datasets to GeoPackage: {self.dst}.")
+
+        try:
+
+            logger.info(f"Creating data source: {self.dst}.")
+
+            # Create GeoPackage.
+            driver = ogr.GetDriverByName("GPKG")
+            gpkg = driver.CreateDataSource(self.dst)
+
+            # Iterate dataframes.
+            for name, df in self.nrn_datasets.items():
+
+                logger.info(f"Layer {name}: creating layer.")
+
+                # Configure layer shape type and spatial reference.
+                if isinstance(df, gpd.GeoDataFrame):
+
+                    srs = osr.SpatialReference()
+                    srs.ImportFromEPSG(df.crs.to_epsg())
+
+                    if len(df.geom_type.unique()) > 1:
+                        raise ValueError(f"Multiple geometry types detected for dataframe {name}: "
+                                         f"{', '.join(map(str, df.geom_type.unique()))}.")
+                    elif df.geom_type[0] in {"Point", "MultiPoint", "LineString", "MultiLineString"}:
+                        shape_type = attrgetter(f"wkb{df.geom_type[0]}")(ogr)
+                    else:
+                        raise ValueError(f"Invalid geometry type(s) for dataframe {name}: "
+                                         f"{', '.join(map(str, df.geom_type.unique()))}.")
+                else:
+                    shape_type = ogr.wkbNone
+                    srs = None
+
+                # Create layer.
+                layer = gpkg.CreateLayer(name=name, srs=srs, geom_type=shape_type, options=["OVERWRITE=YES"])
+
+                logger.info(f"Layer {name}: configuring schema.")
+
+                # Configure layer schema (field definitions).
+                ogr_field_map = {"f": ogr.OFTReal, "i": ogr.OFTInteger, "O": ogr.OFTString}
+
+                for field_name, dtype in df.dtypes.items():
+                    if field_name != "geometry":
+                        field_defn = ogr.FieldDefn(field_name, ogr_field_map[dtype.kind])
+                        layer.CreateField(field_defn)
+
+                # Write layer.
+                layer.StartTransaction()
+
+                for feat in tqdm(df.itertuples(index=False), total=len(df), desc=f"Layer {name}: writing to file"):
+
+                    # Instantiate feature.
+                    feature = ogr.Feature(layer.GetLayerDefn())
+
+                    # Set feature properties.
+                    properties = feat._asdict()
+                    for prop in set(properties) - {"geometry"}:
+                        field_index = feature.GetFieldIndex(prop)
+                        feature.SetField(field_index, properties[prop])
+
+                    # Set feature geometry, if required.
+                    if srs:
+                        geom = ogr.CreateGeometryFromWkb(properties["geometry"].wkb)
+                        feature.SetGeometry(geom)
+
+                    # Create feature.
+                    layer.CreateFeature(feature)
+
+                    # Clear pointer for next iteration.
+                    feature = None
+
+                layer.CommitTransaction()
+
+        except (Exception, KeyError, ValueError, sqlite3.Error) as e:
+            logger.exception(f"Error raised when writing to GeoPackage: {self.dst}.")
+            logger.exception(e)
+            sys.exit(1)
+
     def execute(self):
         """Executes class functionality."""
 
-        # ...
+        self.compile_source_datasets()
+        self.configure_valid_records()
+        self.export_gpkg()
 
 
 @click.command()
