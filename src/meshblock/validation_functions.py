74a62bdff657cc6222d139b6656268340b217add
Jesse Stewart <jesse-stewart18@hotmail.com>
2021-11-05 16:44:14 -0300

    [1] finished implementing 100 series meshblock validations.

--- src/meshblock/validation_functions.py
+++ src/meshblock/validation_functions.py
@@ -6,14 +6,10 @@ import sys
 import uuid
 from collections import defaultdict
 from copy import deepcopy
-from functools import reduce
-from itertools import chain, compress
+from itertools import chain
 from operator import attrgetter, itemgetter
 from pathlib import Path
-from scipy.spatial.distance import euclidean
-from shapely.geometry import LineString, MultiLineString, MultiPoint, Point
-from shapely.ops import split
-from typing import List, Tuple, Union
+from shapely.geometry import Point
 
 sys.path.insert(1, str(Path(__file__).resolve().parents[1]))
 import helpers
@@ -48,9 +44,21 @@ class Validator:
         self.integration_progress = dict()
         self.id = "segment_id"
         self._export = False
+        self._nrn_bos_nodes = None
+        self._nrn_roads_nodes_lookup = dict()
+        self._nrn_bos_nodes_lookup = dict()
+        self._ngd_nodes_lookup = dict()
+
+        # BO integration flag.
+        self._integrated = None
 
         # Resolve identifiers.
         self._update_ids()
+
+        # Separate nrn BOs and roads.
+        self.nrn_roads = self.nrn.loc[(self.nrn["segment_type"] == 1) |
+                                      (self.nrn["segment_type"].isna())].copy(deep=True)
+        self.nrn_bos = self.nrn.loc[self.nrn["segment_type"] == 3].copy(deep=True)
 
         logger.info("Configuring validations.")
 
@@ -61,10 +69,10 @@ class Validator:
                   "desc": "All BOs must have nodal connections to other arcs."},
             101: {"func": self.connectivity_integrated,
                   "desc": "BO node connects to NRN node."},
-            102: {"func": self.connectivity_unintegrated_no_connection,
-                  "desc": "BO node does not have any connections."},
-            103: {"func": self.connectivity_unintegrated_bo_connection,
-                  "desc": "BO node only connects to another BO node."},
+            102: {"func": self.connectivity_unintegrated,
+                  "desc": "BO node does not connect to NRN node."},
+            103: {"func": self.connectivity_nrn_proximity,
+                  "desc": "Unintegrated BO node is <= 5 meters from an NRN road (entire arc)."},
             201: {"func": self.representation_existence,
                   "desc": "All BO ngd_uids must exist."},
             202: {"func": self.representation_duplication,
@@ -72,7 +80,7 @@ class Validator:
         }
 
         # Define validation thresholds.
-        self._bo_ngd_proximity = 5
+        self._bo_nrn_proximity = 5
 
     def _update_ids(self, index: bool = True) -> None:
         """
@@ -111,51 +119,144 @@ class Validator:
             logger.exception(e)
             sys.exit(1)
 
-    def connectivity(self) -> None:
+    def connectivity(self) -> dict:
         """
         Validation: All BOs must have nodal connections to other arcs.
         Note: The output of each connectivity validation feeds into the next. Therefore, this method exists to generate
         the dependant variables for various connectivity validations and is not intended to produce error logs itself.
+
+        :return dict: placeholder dict based on standard validations. For this method, its contents will be unpopulated.
         """
 
-        # TODO
+        errors = {"values": list(), "query": None}
+
+        # Extract nodes.
+        self.nrn_roads["nodes"] = self.nrn_roads["geometry"].map(
+            lambda g: tuple(set(itemgetter(0, -1)(attrgetter("coords")(g)))))
+        self.nrn_bos["nodes"] = self.nrn_roads["geometry"].map(
+            lambda g: tuple(set(itemgetter(0, -1)(attrgetter("coords")(g)))))
+        self.ngd["nodes"] = self.ngd["geometry"].map(
+            lambda g: tuple(set(itemgetter(0, -1)(attrgetter("coords")(g)))))
+
+        # Explode nodes collections and group identifiers based on shared nodes.
+        nrn_roads_nodes_exp = self.nrn_roads["nodes"].explode()
+        self._nrn_roads_nodes_lookup = dict(helpers.groupby_to_list(
+            pd.DataFrame({"node": nrn_roads_nodes_exp.values, self.id: nrn_roads_nodes_exp.index}),
+            group_field="node", list_field=self.id).map(tuple))
+
+        nrn_bos_nodes_exp = self.nrn_bos["nodes"].explode()
+        self._nrn_bos_nodes_lookup = dict(helpers.groupby_to_list(
+            pd.DataFrame({"node": nrn_bos_nodes_exp.values, self.id: nrn_bos_nodes_exp.index}),
+            group_field="node", list_field=self.id).map(tuple))
+
+        ngd_nodes_exp = self.ngd["nodes"].explode()
+        self._ngd_nodes_lookup = dict(helpers.groupby_to_list(
+            pd.DataFrame({"node": ngd_nodes_exp.values, self.id: ngd_nodes_exp.index}),
+            group_field="node", list_field=self.id).map(tuple))
+
+        # Explode BO node collections to allow for individual node validation.
+        self._nrn_bos_nodes = self.nrn_bos["nodes"].explode().copy(deep=True)
+
+        # Populate progress tracker with total BO node count.
+        self.integration_progress["total BO nodes"] = len(self._nrn_bos_nodes)
+
+        return errors
 
     def connectivity_integrated(self) -> dict:
         """
         Validates: BO node connects to NRN node.
 
-        :return dict: dict containing error messages and, optionally, a query to identify erroneous records.
+        :return dict: placeholder dict based on standard validations. For this method, its contents will be unpopulated.
         """
 
         errors = {"values": list(), "query": None}
 
-        # TODO
+        # Flag BO nodes connected to an nrn road node.
+        self._integrated = self._nrn_bos_nodes.map(lambda node: node in self._nrn_roads_nodes_lookup)
+
+        # Populate progress tracker with BO node count.
+        self.integration_progress["integrated BO nodes"] = sum(self._integrated)
 
         return errors
 
-    def connectivity_unintegrated_bo_connection(self) -> dict:
+    def connectivity_nrn_proximity(self) -> dict:
         """
-        Validates: BO node only connects to another BO node.
+        Validates: Unintegrated BO node is <= 5 meters from an NRN road (entire arc).
 
         :return dict: dict containing error messages and, optionally, a query to identify erroneous records.
         """
 
         errors = {"values": list(), "query": None}
 
-        # TODO
+        # Compile unintegrated BO nodes.
+        unintegrated_bo_nodes = pd.Series(tuple(set(self._nrn_bos_nodes.loc[~self._integrated])))
+
+        # Generate simplified node buffers with distance tolerance.
+        node_buffers = unintegrated_bo_nodes.map(lambda node: Point(node).buffer(self._bo_nrn_proximity, resolution=5))
+
+        # Query nrn roads which intersect each node buffer.
+        node_intersects = node_buffers.map(
+            lambda buffer: set(self.nrn_roads.sindex.query(buffer, predicate="intersects")))
+
+        # Filter unintegrated bo nodes to those with buffers with one or more intersecting nrn roads.
+        unintegrated_bo_nodes = unintegrated_bo_nodes.loc[node_intersects.map(len) >= 1]
+        if len(unintegrated_bo_nodes):
+
+            # Compile identifiers of arcs for resulting BO nodes.
+            vals = tuple(chain.from_iterable(unintegrated_bo_nodes.map(self._nrn_bos_nodes_lookup).values))
+
+            # Compile error logs.
+            errors["values"] = set(vals)
+            errors["query"] = f"\"{self.id}\" in {*set(vals),}"
+
+            # Populate progress tracker with BO node count.
+            self.integration_progress["unintegrated BO nodes within nrn proximity"] = len(vals)
 
         return errors
 
-    def connectivity_unintegrated_no_connection(self) -> dict:
+    def connectivity_unintegrated(self) -> dict:
         """
-        Validates: BO node does not have any connections.
+        Validates: BO node does not connect to NRN node.
+        A) BO node connects only to NGD road node.
+        B) BO node connects only to another BO node.
+        C) BO node connects only to NGD road node and another BO node.
+        D) BO is isolated (i.e. connected only to itself).
 
-        :return dict: dict containing error messages and, optionally, a query to identify erroneous records.
+        :return dict: placeholder dict based on standard validations. For this method, its contents will be unpopulated.
         """
 
         errors = {"values": list(), "query": None}
 
-        # TODO
+        # A) Flag BO nodes that connect only to NGD road nodes.
+        flag_ngd_connection = (~self._integrated) & (self._nrn_bos_nodes.map(
+            lambda node: (node in self._ngd_nodes_lookup) & (len(itemgetter(node)(self._nrn_bos_nodes_lookup)) == 1)))
+
+        # Populate progress tracker with BO node count.
+        self.integration_progress["unintegrated BO nodes (ngd road connection)"] = sum(flag_ngd_connection)
+
+        # B) Flag BO nodes that connect only to another BO node.
+        flag_bo_connection = (~self._integrated) & (self._nrn_bos_nodes.map(
+            lambda node: (node not in self._ngd_nodes_lookup) &
+                         (len(itemgetter(node)(self._nrn_bos_nodes_lookup)) > 1)))
+
+        # Populate progress tracker with BO node count.
+        self.integration_progress["unintegrated BO nodes (bo road connection)"] = sum(flag_bo_connection)
+
+        # C) Flag BO nodes that connect only to NGD road nodes and another BO node.
+        flag_ngd_and_bo_connection = (~self._integrated) & (self._nrn_bos_nodes.map(
+            lambda node: (node in self._ngd_nodes_lookup) & (len(itemgetter(node)(self._nrn_bos_nodes_lookup)) > 1)))
+
+        # Populate progress tracker with BO node count.
+        self.integration_progress["unintegrated BO nodes (ngd road and bo connection)"] =\
+            sum(flag_ngd_and_bo_connection)
+
+        # D) Flag BO nodes that are isolated (i.e. connected only to itself).
+        flag_no_connection = (~self._integrated) & (self._nrn_bos_nodes.map(
+            lambda node: (node not in self._ngd_nodes_lookup) &
+                         (len(itemgetter(node)(self._nrn_bos_nodes_lookup)) == 1)))
+
+        # Populate progress tracker with BO node count.
+        self.integration_progress["unintegrated BO nodes (no connection)"] = sum(flag_no_connection)
 
         return errors
 
