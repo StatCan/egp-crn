1ee85db452c074e3d384db7b9765fbdd4de1cd3b
Jesse Stewart <jesse-stewart18@hotmail.com>
2021-12-14 12:16:19 -0400

    [1] restore geometry now uses linemerge instead of unary_union and includes a refinement to reduce non-matches based on length; [2] added export and log of restore geometry results.

--- src/restore_geometry.py
+++ src/restore_geometry.py
@@ -2,10 +2,11 @@ import click
 import fiona
 import geopandas as gpd
 import logging
-import re
 import sys
-from operator import itemgetter
+from operator import attrgetter, itemgetter
 from pathlib import Path
+from shapely.geometry import LineString
+from shapely.ops import linemerge
 from tabulate import tabulate
 
 
@@ -37,10 +38,14 @@ class EGPRestoreGeometry:
         self.layer = f"nrn_bo_{source}"
         self.nrn_id = "segment_id_orig"
         self.bo_id = "ngd_uid"
-        self.src = Path(filepath.parents[2] / "data/interim/egp_data.gpkg")
-        self.src_restore = Path(filepath.parents[2] / "data/interim/nrn_bo_restore.gpkg")
+        self.src = Path(filepath.parents[1] / "data/interim/egp_data.gpkg")
+        self.src_restore = Path(filepath.parents[1] / "data/interim/nrn_bo_restore.gpkg")
         self.modified_nrn = set()
         self.modified_bo = set()
+
+        # Define thresholds.
+        self._rnd_prec = 2
+        self._len_prec = 0
 
         # Configure source path and layer name.
         for src in (self.src, self.src_restore):
@@ -62,7 +67,18 @@ class EGPRestoreGeometry:
         self.df_restore = gpd.read_file(self.src_restore, layer=self.layer)
         logger.info("Successfully loaded source restoration data.")
 
-    def restore_data(self) -> None:
+        # Round coordinates to defined decimal precision.
+        # Note: This accounts for point snapping which, within a threshold, should not be considered a modification.
+        logger.info(f"Rounding coordinates to decimal precision: {self._rnd_prec} (0.{'0'*(self._rnd_prec-1)}1).")
+
+        self.df["geometry"] = self.df["geometry"].map(lambda g: LineString(map(
+            lambda pt: [round(itemgetter(0)(pt), self._rnd_prec), round(itemgetter(1)(pt), self._rnd_prec)],
+            attrgetter("coords")(g))))
+        self.df_restore["geometry"] = self.df_restore["geometry"].map(lambda g: LineString(map(
+            lambda pt: [round(itemgetter(0)(pt), self._rnd_prec), round(itemgetter(1)(pt), self._rnd_prec)],
+            attrgetter("coords")(g))))
+
+    def identify_mods(self) -> None:
         """Identifies partially or completely missing restoration data geometries from the source dataset."""
 
         logger.info("Identifying missing data.")
@@ -74,8 +90,10 @@ class EGPRestoreGeometry:
         flag_bo_restore = self.df_restore["segment_type"].astype(str).isin({"3", "3.0"})
 
         # Filter to exclusively nrn and bo arcs and dissolve geometries on identifiers.
-        nrn_dissolved = self.df.loc[flag_nrn].dissolve(by=self.nrn_id, sort=False)
-        bo_dissolved = self.df.loc[flag_bo].dissolve(by=self.bo_id, sort=False)
+        nrn_dissolved = helpers.groupby_to_list(self.df.loc[flag_nrn], group_field=self.nrn_id, list_field="geometry")\
+            .map(lambda geoms: geoms[0] if len(geoms) == 1 else linemerge(geoms))
+        bo_dissolved = helpers.groupby_to_list(self.df.loc[flag_bo], group_field=self.bo_id, list_field="geometry")\
+            .map(lambda geoms: geoms[0] if len(geoms) == 1 else linemerge(geoms))
 
         # Create identifier - geometry lookups for new geometries.
         nrn_id_geom_lookup = dict(zip(nrn_dissolved.index, nrn_dissolved["geometry"]))
@@ -86,28 +104,47 @@ class EGPRestoreGeometry:
         flag_bo_link = self.df_restore[self.bo_id].isin(bo_id_geom_lookup)
 
         # Compile new arc associated with each original arc.
-        self.df_restore["geometry_orig"] = None
-        self.df_restore.loc[flag_nrn_link, "geometry_orig"] = self.df_restore.loc[flag_nrn_link, self.nrn_id]\
+        self.df_restore["geometry_new"] = None
+        self.df_restore.loc[flag_nrn_link, "geometry_new"] = self.df_restore.loc[flag_nrn_link, self.nrn_id]\
             .map(lambda val: itemgetter(val)(nrn_id_geom_lookup))
-        self.df_restore.loc[flag_bo_link, "geometry_orig"] = self.df_restore.loc[flag_bo_link, self.bo_id]\
+        self.df_restore.loc[flag_bo_link, "geometry_new"] = self.df_restore.loc[flag_bo_link, self.bo_id]\
             .map(lambda val: itemgetter(val)(bo_id_geom_lookup))
 
         # Validate geometry equality.
         self.df_restore["equals"] = False
-        self.df_restore.loc[~self.df_restore["geometry_orig"].isna(), "equals"] = \
-            self.df_restore.loc[~self.df_restore["geometry_orig"].isna(), ["geometry", "geometry_orig"]]\
+        self.df_restore.loc[~self.df_restore["geometry_new"].isna(), "equals"] = \
+            self.df_restore.loc[~self.df_restore["geometry_new"].isna(), ["geometry", "geometry_new"]]\
                 .apply(lambda row: row[0].equals(row[1]), axis=1)
+
+        # Refinement 1) update equality status based on geometry length, rounded to a defined decimal precision.
+        refinement_flag = ~(self.df_restore["equals"] | self.df_restore["geometry_new"].isna())
+        self.df_restore.loc[refinement_flag, "equals"] =\
+            self.df_restore.loc[refinement_flag, ["geometry", "geometry_new"]].apply(
+                lambda row: round(row[0].length, self._len_prec) == round(row[1].length, self._len_prec), axis=1)
 
         # Store identifiers of modified arcs.
         self.modified_nrn.update(set(self.df_restore.loc[flag_nrn_restore & (~self.df_restore["equals"]), self.nrn_id]))
         self.modified_bo.update(set(self.df_restore.loc[flag_bo_restore & (~self.df_restore["equals"]), self.bo_id]))
 
-        # TODO: refine equals criteria... perhaps dont include those with the same approximate length...
+    def restore_and_log_mods(self) -> None:
+        """Exports records of modified geometries and logs results."""
+
+        logger.info(f"Restoring and logging modified data.")
+
+        # Compile records of modified geometries.
+        export_df = self.df_restore.loc[~self.df_restore["equals"]].copy(deep=True)
+        helpers.export(export_df, dst=self.src, name=f"restore_{self.source}")
+
+        # Log modification summary.
+        table = tabulate([["NRN", len(self.modified_nrn)], ["BO", len(self.modified_bo)]],
+                         headers=["Arc Type", "Count"], tablefmt="rst", colalign=("left", "right"))
+        logger.info("Summary of restored data:", "\n" + table)
 
     def execute(self) -> None:
         """Executes the EGP class."""
 
-        self.restore_data()
+        self.identify_mods()
+        self.restore_and_log_mods()
 
 
 @click.command()
