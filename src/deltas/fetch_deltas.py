f2e764c55ee6bb2f50017f0f007968d20eea8c6a
Hazem Elmalik <hazem.elmalik@statcan.gc.ca>
2022-10-03 12:27:52 -0400

    [1] updated source gpkg name (now crn.gpkg) and source layer name (now crn_{source}); [2] added bloat filter to NRN deletions; [3] added NGD deletions to the NGD deltas process; [4] combined source data standardization and filtration steps; [5] replaced region codes with  province codes for script usage.

--- src/deltas/fetch_deltas.py
+++ src/deltas/fetch_deltas.py
@@ -17,7 +17,7 @@ filepath = Path(__file__).resolve()
 sys.path.insert(1, str(Path(__file__).resolve().parents[1]))
 
 
-# import hazhelper
+import dltshelper
 import helpers
 
 # Set logger.
@@ -48,18 +48,20 @@ class CRNDeltas:
         self.mode = mode
         self.process_ngd = mode in {"both", "ngd"}
         self.process_nrn = mode in {"both", "nrn"}
-
-        self.layer = f"nrn_bo_{source}"
+        self.layer = f"crn_{source}"
         self.layer_ngd = f"ngd_al_{source}"
+        self.layer_ngd_current = f"ngd_al_current_{source}"
         self.layer_nrn = f"nrn_{source}"
-
         self.id = "segment_id"
         self.ngd_id = "ngd_uid"
         self.nrn_id = "nid"
-
-        self.src = Path(filepath.parents[2] / "data/egp_data.gpkg")
+        self.src = Path(filepath.parents[2] / "data/crn.gpkg")
         self.src_ngd = Path(filepath.parents[2] / "data/ngd_al.gpkg")
+        self.src_ngd_current = Path(filepath.parents[2] / "data/ngd_al_current.gpkg")
         self.src_nrn = Path(filepath.parents[2] / "data/nrn.gpkg")
+        self.del_nrn = None
+        self.add_nrn = None
+        self.nrn_deltas = None
 
         # Configure source path and layer name.
         for src in (self.src, self.src_ngd, self.src_nrn):
@@ -77,22 +79,29 @@ class CRNDeltas:
         self.crn = gpd.read_file(self.src, layer=self.layer)
         logger.info("Successfully loaded source data.")
 
-        # Standardize data and snap nodes.
-        print(Counter(self.crn.segment_type))
+        # Standardize crn data and filter to roads or BOs.
+        logger.info("Standardizing CRN data")
+        crn_identifier = "segment_id_orig"
         self.crn = helpers.standardize(self.crn)
-        print(Counter(self.crn.segment_type))
         self.crn = helpers.snap_nodes(self.crn)
+        self.crn_rd = self.crn.loc[(self.crn["segment_type"] == 1) &
+                                   (self.crn[crn_identifier].map(len) == 32)].copy(deep=True)
+        self.crn_added = self.crn.loc[self.crn[crn_identifier].map(len) != 32].copy(deep=True)
+        logger.info("Finished standardizing CRN data")
 
         # Load NGD data.
         if self.process_ngd:
             logger.info(f"Loading NGD data: {self.src_ngd}|layer={self.layer_ngd}.")
             self.ngd = gpd.read_file(self.src_ngd, layer=self.layer_ngd)
+            self.ngd_current = gpd.read_file(self.src_ngd_current, layer=self.layer_ngd_current)
             logger.info("Successfully loaded NGD data.")
 
-            # Standardize data.
-            # self.ngd = hazhelper.drop_zero_geom(self.ngd)
-            # self.ngd = helpers.round_coordinates(self.ngd)
-            # self.ngd["SGMNT_DTE"] = self.ngd.SGMNT_DTE.dt.strftime("%Y%m%d").astype(int)
+            # Standardize and filter NGD data.
+            logger.info("Standardizing NGD data.")
+            self.ngd = dltshelper.drop_zero_geom(self.ngd)
+            self.ngd = helpers.round_coordinates(self.ngd)
+            self.ngd["SGMNT_DTE"] = self.ngd.SGMNT_DTE.dt.strftime("%Y%m%d").astype(int)
+            logger.info("Finished standardizing NGD data.")
 
         # Load NRN data.
         if self.process_nrn:
@@ -100,9 +109,11 @@ class CRNDeltas:
             self.nrn = gpd.read_file(self.src_nrn, layer=self.layer_nrn)
             logger.info("Successfully loaded NRN data.")
 
-            # Standardize data.
+            # Standardize NRN data
+            logger.info("Standardizing NRN data.")
             self.nrn = self.nrn.to_crs(3347)
             self.nrn = helpers.round_coordinates(self.nrn)
+            logger.info("Finished standardizing NRN data.")
 
     def __call__(self) -> None:
         """Executes the CRN class."""
@@ -117,62 +128,83 @@ class CRNDeltas:
 
         logger.info("Fetching NGD deltas.")
 
-        # filter ngd arcs based on date created.
+        # Filter NGD arcs based on date created.
         ngd_additions = set(self.ngd.loc[self.ngd["SGMNT_DTE"] >= 20210601, "geometry"].map
                             (lambda g: attrgetter("coords")(g)))
+        logger.info(f"There were {len(ngd_additions)} features added to the NGD")
 
-        # Create NGD additions GeoDatFrame.
-        ngd_add_gdf = gpd.GeoDataFrame(geometry=list(map(LineString, ngd_additions)), crs=self.crn.crs)
+        # Extract NGD identifiers as sets.
+        ngd_current_ids = set(self.ngd_current["ngd_uid"])
+        ngd_al_ids = set(self.ngd["NGD_UID"])
 
-        # Export NGD additions GeoDataFrame to GeoPackage
-        helpers.export(ngd_add_gdf, dst=self.src, name=f"{self.source}_ngd_deltas")
+        # Configure NGD deletions.
+        ngd_del_ids = ngd_current_ids - ngd_al_ids
+        logger.info("Finished listing deleted ids")
+        logger.info(f"There were {len(ngd_del_ids)} features deleted from the NGD")
+
+        # Extract NGD deleted arcs and create NGD deletions set.
+        ngd_del_arcs = self.ngd_current.loc[self.ngd_current["ngd_uid"].isin(ngd_del_ids)].copy(deep=True)
+        ngd_deletions = set(ngd_del_arcs["geometry"].map(lambda g: attrgetter("coords")(g)))
+
+        # Create NGD deltas GeoDatFrames.
+        ngd_add = gpd.GeoDataFrame(geometry=list(map(LineString, ngd_additions)), crs=self.crn.crs)
+        ngd_add["status"] = "add"
+        ngd_del = gpd.GeoDataFrame(geometry=list(map(LineString, ngd_deletions)), crs=self.crn.crs)
+        ngd_del["status"] = "delete"
+
+        # Merge NGD deltas GeoDataFrames.
+        ngd_deltas = ngd_del.merge(ngd_add, on="geometry", how="outer", suffixes=("_del", "_add"))
+
+        # Compile NGD delta classifications.
+        ngd_deltas["status"] = -1
+        ngd_deltas.loc[ngd_deltas["status_del"].isna(), "status"] = "Addition"
+        ngd_deltas.loc[ngd_deltas["status_add"].isna(), "status"] = "Deletion"
+        ngd_deltas = ngd_deltas.loc[ngd_deltas["status"] != -1].fillna(0).copy(deep=True)
+
+        # Export NGD deltas GeoDataFrame to GeoPackage
+        if len(ngd_deltas):
+            helpers.export(ngd_deltas, dst=self.src, name=f"{self.source}_ngd_deltas")
 
     def fetch_nrn_deltas(self) -> None:
         """Identifies and retrieves NRN deltas."""
 
         logger.info("Fetching NRN deltas.")
 
-        # Compile all nrn and crn road points as sets.
+        # Compile CRN road and BO and NRN vertices as sets.
         nrn_nodes = set(self.nrn["geometry"].map(lambda g: attrgetter("coords")(g)).explode())
-        crn_nodes = set(self.crn.loc[self.crn["segment_type" == 1], "geometry"].map(
-            lambda g: attrgetter("coords")(g)).explode())
+        crn_nodes = set(self.crn_rd["geometry"].map(lambda g: attrgetter("coords")(g)).explode())
+        crn_added_nodes = set(self.crn_added["geometry"].map(lambda g: attrgetter("coords")(g)).explode())
 
-        # Configure deltas.
+        # Configure NRN deltas.
         additions = nrn_nodes - crn_nodes
         deletions = crn_nodes - nrn_nodes
+        deletions_fltr = deletions - crn_added_nodes
 
-        del_gdf = gpd.GeoDataFrame(geometry=list(map(Point, deletions)), crs=self.crn.crs)
-        add_gdf = gpd.GeoDataFrame(geometry=list(map(Point, additions)), crs=self.crn.crs)
+        # Create NRN deltas GeoDataFrames.
+        self.del_nrn = gpd.GeoDataFrame(geometry=list(map(Point, deletions_fltr)), crs=self.crn.crs)
+        self.del_nrn["status"] = "delete"
+        self.add_nrn = gpd.GeoDataFrame(geometry=list(map(Point, additions)), crs=self.crn.crs)
+        self.add_nrn["status"] = "add"
 
-        # Create DataFrames from delta pt tuples.
-        del_df = pd.DataFrame({"del_status": "delete", "geometry": del_gdf["geometry"].map(
-                                      lambda g: (attrgetter("coords")(g)))})
-        add_df = pd.DataFrame({"add_status": "add", "geometry": add_gdf["geometry"].map(
-                                      lambda g: (attrgetter("coords")(g)))})
-
-        # Merge NRN deltas dataframes.
-        nrn_deltas = del_df.merge(add_df, on="geometry", how="outer")
+        # Merge NRN deltas GeoDataframes.
+        nrn_deltas = self.del_nrn.merge(self.add_nrn, on="geometry", how="outer", suffixes=("_del", "_add"))
 
         # Compile NRN delta classifications.
         nrn_deltas["status"] = -1
-        nrn_deltas.loc[nrn_deltas["del_status"].isna(), "status"] = "addition"
-        nrn_deltas.loc[nrn_deltas["add_status"].isna(), "status"] = "deletion"
+        nrn_deltas.loc[nrn_deltas["status_del"].isna(), "status"] = "addition"
+        nrn_deltas.loc[nrn_deltas["status_add"].isna(), "status"] = "deletion"
+        nrn_deltas = nrn_deltas.loc[nrn_deltas["status"] != -1].fillna(0).copy(deep=True)
 
-        # Create NRN deltas GeoDataFrame.
-        nrn_deltas = nrn_deltas.loc[nrn_deltas["status"] != -1].fillna("other").copy(deep=True)
+        # Export NRN deltas GeoDataFrame to GeoPackage.
         if len(nrn_deltas):
-            deltas_nrn = gpd.GeoDataFrame(nrn_deltas, geometry=list(map(Point, nrn_deltas["geometry"])),
-                                          crs=self.crn.crs)
+            helpers.export(nrn_deltas, dst=self.src, name=f"{self.source}_nrn_deltas")
 
-        # Export delta GeoDataFrames to GeoPackages.
-        helpers.export(deltas_nrn, dst=self.src, name=f"{self.source}_nrn_deltas_T")
-        # helpers.export(add_gdf, dst=self.src, name=f"{self.source}_additions")
-
-        # TODO: [1] revisit fetch_nrn_deltas() and fetch_ngd_deltas; [2] create error logger.
+    # TODO: create progress logger.
 
 
 @click.command()
-@click.argument("source", type=click.Choice(helpers.load_yaml("../config.yaml")["sources"], False))
+@click.argument("source", type=click.Choice(["ab", "bc", "mb", "nb", "nl", "nt", "ns", "nu",
+                                            "on", "pe", "qc", "sk", "yt"], False))
 @click.option("--mode", "-m", type=click.Choice(["both", "ngd", "nrn"], False), default="both", show_default=True,
               help="The type of deltas to be returned.")
 def main(source: str, mode: str = "both") -> None:
