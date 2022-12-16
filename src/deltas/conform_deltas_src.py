8bfffaa9752fb2e1e55d7e9d0b75f8c83a59f605
Jesse Stewart <jesse-stewart18@hotmail.com>
2022-12-16 16:40:41 -0400

    [1] added src parameter and some todo placeholders for conform_deltas_src.py

--- src/deltas/conform_deltas_src.py
+++ src/deltas/conform_deltas_src.py
@@ -23,32 +23,44 @@ logger.addHandler(handler)
 class CRNDeltas:
     """Defines the CRN deltas class."""
 
-    def __init__(self, source: str, mode: str) -> None:
+    def __init__(self, src: Path, source: str, mode: str) -> None:
         """
         Initializes the CRN class.
 
         \b
+        :param Path src: path to the source GeoPackage (NRN) or ESRI File Geodatabase (NGD).
         :param str source: code for the source province / territory.
         :param mode: the type of deltas to be returned: {'ngd', 'nrn'}.
         """
 
+        self.src = src
         self.source = source
         self.mode = mode
+
+        # Validate src.
+        # TODO: ...
+
+        # Validate and create dst.
 
     def __call__(self) -> None:
         """Executes the CRN class."""
 
-        # TODO
+        # TODO: find input layers and load data
+        # TODO: for nrn data, create nrn_full_attribution layer (maybe I can skip this since we dont know which records are deltas at this point)
+        # TODO: create simplified NRN / NGD data which resembled CRN schema
+        # Export (new gpkg in src location.
 
 
 @click.command()
+@click.argument("src", type=click.Path(exists=True, dir_okay=False, resolve_path=True, path_type=Path))
 @click.argument("source", type=click.Choice("ab bc mb nb nl ns nt nu on pe qc sk yt".split(), False))
 @click.argument("mode", type=click.Choice(["ngd", "nrn"], False))
-def main(source: str, mode: str) -> None:
+def main(src: Path, source: str, mode: str) -> None:
     """
     Instantiates and executes the CRN class.
 
     \b
+    :param Path src: path to the source GeoPackage (NRN) or ESRI File Geodatabase (NGD).
     :param str source: code for the source province / territory.
     :param str mode: the type of deltas to be returned: {'ngd', 'nrn'}.
     """
@@ -56,7 +68,7 @@ def main(source: str, mode: str) -> None:
     try:
 
         with helpers.Timer():
-            deltas = CRNDeltas(source, mode)
+            deltas = CRNDeltas(src, source, mode)
             deltas()
 
     except KeyboardInterrupt:
